0d3b03a7959fd4ba65e1b524142420c1b6e8e7e6
Jyoti <jyotime6678@gmail.com>
2019-06-21 12:34:43 +0530

    DYnamic Content with input boxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 import {  FileUploader, FileSelectDirective } from 'ng2-file-upload/ng2-file-upload';
 
 // const URL = 'http://localhost:3000/api/upload';
-const URL = 'http://3.17.180.228:8000/api/v1/file/';
+// const URL = 'http://3.17.180.228:8000/api/v1/';
+const URL = 'http://127.0.0.1:8000/api/v1/';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +13,34 @@ const URL = 'http://3.17.180.228:8000/api/v1/file/';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'file-upload';
-  public uploader: FileUploader = new FileUploader({url: URL, itemAlias: 'file'});
+	title = 'file-upload';
+	final:any;
+	webdata:any;
+	error:any;
 
-  ngOnInit() {
-    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
-    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
-         console.log('ImageUpload:uploaded:', item, status, response);
-         alert('File uploaded successfully');
-     };
- }
+	constructor(private httpClient: HttpClient, private sanitizer: DomSanitizer){
+		this.oldfile().subscribe(
+		  (res) => {
+			this.webdata = res;
+			this.final = sanitizer.bypassSecurityTrustHtml(this.webdata.html);
+			// console.log(res);
+		  },
+		  (err) => this.error = err
+		);
+	}
+
+	public oldfile() {
+		let url = URL+`file/1/`;
+		return this.httpClient.get(url);
+	}
+
+	public uploader: FileUploader = new FileUploader({url: URL+'file/', itemAlias: 'file'});
+
+	ngOnInit() {
+		this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+		this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+			 console.log('ImageUpload:uploaded:', item, status, response);
+			 alert('File uploaded successfully');
+		 };
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
